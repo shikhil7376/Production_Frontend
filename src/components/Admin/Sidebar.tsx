a94cd2a4215887c0268a71f055d65dde663b8554
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-02 10:28:39 +0530

    change link in sidebar of admin

--- src/components/Admin/Sidebar.tsx
+++ src/components/Admin/Sidebar.tsx
@@ -1,34 +1,45 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import React from 'react';
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiChartPie, HiUser, HiLibrary  } from "react-icons/hi";
+import { HiArrowSmRight, HiChartPie, HiUser, HiLibrary } from "react-icons/hi";
 import { MdReportProblem } from "react-icons/md";
 
 const AdminSidebar = () => {
   return (
-    <div className='mt-8  h-screen '>
-    <Sidebar aria-label="Default sidebar example" className='w-[230px] rounded-2xl'>
-   <Sidebar.Items className="mt-8  ">
-     <Sidebar.ItemGroup>
-       <Sidebar.Item href="/admin/dashboard" icon={HiChartPie}>
-       <p className='text-small font-semibold'> Dashboard</p>
-       </Sidebar.Item>
-       <Sidebar.Item href="/admin/users" icon={HiUser}>
-       <p className='text-small font-semibold'> Users</p>
-       </Sidebar.Item>
-       <Sidebar.Item href="/admin/kennelapproval" icon={ HiLibrary } labelColor="dark">
-       <p className='text-small font-semibold'>Kennel Approval</p>
-       </Sidebar.Item>
-       <Sidebar.Item href="/admin/verifiedKennelOwner" icon={HiArrowSmRight}>
-          <p className='text-small font-semibold'>Kennel Owners</p>
-       </Sidebar.Item>
-       <Sidebar.Item href="/admin/postreports" icon={ MdReportProblem }>
-          <p className='text-small font-semibold'>Post Reports</p>
-       </Sidebar.Item>
-     </Sidebar.ItemGroup>
-   </Sidebar.Items>
- </Sidebar>
- </div>
-  )
-}
+    <div className='mt-8 h-screen'>
+      <Sidebar aria-label="Default sidebar example" className='w-[230px] rounded-2xl'>
+        <Sidebar.Items className="mt-8">
+          <Sidebar.ItemGroup>
+            <Sidebar.Item icon={HiChartPie}>
+              <Link to="/admin/dashboard" className="text-small font-semibold">
+                Dashboard
+              </Link>
+            </Sidebar.Item>
+            <Sidebar.Item icon={HiUser}>
+              <Link to="/admin/users" className="text-small font-semibold">
+                Users
+              </Link>
+            </Sidebar.Item>
+            <Sidebar.Item icon={HiLibrary}>
+              <Link to="/admin/kennelapproval" className="text-small font-semibold">
+                Kennel Approval
+              </Link>
+            </Sidebar.Item>
+            <Sidebar.Item icon={HiArrowSmRight}>
+              <Link to="/admin/verifiedKennelOwner" className="text-small font-semibold">
+                Kennel Owners
+              </Link>
+            </Sidebar.Item>
+            <Sidebar.Item icon={MdReportProblem}>
+              <Link to="/admin/postreports" className="text-small font-semibold">
+                Post Reports
+              </Link>
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+    </div>
+  );
+};
 
-export default AdminSidebar
+export default AdminSidebar;
